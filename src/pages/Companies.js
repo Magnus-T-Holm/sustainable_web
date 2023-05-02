39cc03d7fa3e7364aadadbb977f66c48851c9999
Magnus Holm <magnusthestrup@hotmail.com>
2023-05-02 23:45:23 +0200

    Update Companies.js

--- src/pages/Companies.js
+++ src/pages/Companies.js
@@ -8,9 +8,17 @@ const Frontpage = () => {
         <section id="adnami">
           <h2>Adnami</h2>
           <p></p>
+          <h3>Accessibility</h3>
+          <p>Har ikke rigtigt noget fokus på accessibility, når det kommer til deres digitale producter, da det er begranset hvad de kan gøre.</p>
+          <h3>Sustainability</h3>
+          <p>Ligsom med accessibility, så er det begranset hvad Adnami kan gøre, ihvert når det kommer til den visuele del. De prøver dog at sørge for at de scripts de bruger er så lette som muligt, så de påvirker siderne så lidt som muligt.</p>
         </section>
         <section id="inochi">
           <h2>Inochi2D</h2>
+          <p></p>
+          <h3>Accessibility</h3>
+          <p></p>
+          <h3>Sustainability</h3>
           <p></p>
         </section>
       </article>
